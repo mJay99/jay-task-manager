eed33d0c8c3b90f2b28fd6a982461e7f699418f3
mJay99 <jayeshmali299@gmail.com>
2020-11-08 23:02:34 +0530

    Semantic auto app version config added.

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import {environment} from '../../../environments/environment';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  public version  = environment.appVersion;
   constructor(   private router: Router,) { }
 
   ngOnInit() {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
+declare var require :any
 export const environment = {
+  appVersion: require('../../package.json').version,
   production: true,
   baseUrl :  "https://devza.com/tests/tasks/"
 };
