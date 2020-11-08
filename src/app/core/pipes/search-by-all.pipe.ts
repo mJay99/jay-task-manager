5b2ee8c23471b40d4b40ff260121cb6c0a060283
mJay99 <jayeshmali299@gmail.com>
2020-11-08 21:59:30 +0530

    Seach By pipe and thereby multi search funtionality enhanced

--- src/app/core/pipes/search-by-all.pipe.ts
+++ src/app/core/pipes/search-by-all.pipe.ts
@@ -1,5 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+import * as moment from 'moment';
+import { Priority } from '../enums/priority.enum';
+// let thisMoment: moment.Moment = moment("someDate");
 @Pipe({
   name: 'searchByAll'
 })
@@ -8,11 +10,24 @@ export class SearchByAllPipe implements PipeTransform {
   transform(items: any, filter: any): any {
     if (filter && Array.isArray(items)) {
       let filterKeys = Object.keys(filter);
-        return items.filter(item => {
-          return filterKeys.some((keyName) => {
-            return new RegExp(filter[keyName], 'gi').test(item[keyName]) || filter[keyName] == "";
-          });
+      return items.filter((item: any) => {
+        let modified_item = { ...item };
+        return filterKeys.some((keyName) => {
+          if (keyName === 'due_date') {
+            let date = moment(modified_item.due_date).format("dddd, MMMM Do YYYY, h:mm:ss a");
+            modified_item.due_date = date;
+          }
+          if (item.priority == Priority.High) {
+            modified_item.priority = 'high'
+          } else if (item.priority == Priority.Medium) {
+            modified_item.priority = 'medium'
+          } else {
+            modified_item.priority = 'normal'
+          }
+      
+          return new RegExp(filter[keyName], 'gi').test(modified_item[keyName]) || filter[keyName] == "";
         });
+      });
     }
   }
 
